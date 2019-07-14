72bad8b6e7af86be8f51ad0c7ab8438bb4750427
teeraphat <teeraphatmychin@gmail.com>
2019-07-14 17:04:03 +0700

    Update detail-car.blade.php

--- resources/views/detail-car.blade.php
+++ resources/views/detail-car.blade.php
@@ -269,7 +269,7 @@
 			</div>
 		</section>
 	
-	@extend('layout.partials.footer')
+	@extends('layout.partials.footer')
 	@extends('layout.partials.footer-scripts')
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
