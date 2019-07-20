5b3b4273a66316ebf194138c9bef4222326adc66
teeraphat <teeraphatmychin@gmail.com>
2019-07-20 22:30:24 +0700

    counter

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -144,7 +144,7 @@
 										<i class="fa fa-car"></i>
 											<div class="text-content">
 												<h6>เรามีโปรโมชั่นรถให้คุณ</h6>
-												<p>เพียงแค่กรอกข้อมูลกับเรา (ฟรี)</p>
+												<p>เพียงแค่กรอกข้อมูลกับเรา (ฟรี)</p><br>
 											</div>
 									</div>
 								</div>
@@ -257,6 +257,49 @@
 				</div>
 			</div>
 	</section>
+
+	<section>
+		<div class="container">
+			<div class="row">
+			<br/>
+				<div class="col text-center">
+					<h1>ความภาคภูมิใจและเกียรติยศ</h1>
+					<p>เราจะพยายามและทำงานอย่างหนักเพื่อความสุขของคุณ</p>
+				</div>						
+			</div>
+			<div class="row text-center">
+				<div class="col">
+					<div class="counter">
+					  	<i class="fas fa-medal"></i>
+					  	<h2 class="timer count-title count-number" data-to="2520" data-speed="5000"></h2>
+					   	<h6 class="count-text ">กว่าโปรโมชั่นมากมายให้เลือก</h6>
+					</div>
+				</div>
+				<div class="col">
+					<div class="counter">
+						<i class="far fa-thumbs-up"></i>
+						<h2 class="timer count-title count-number" data-to="51" data-speed="2000"></h2>
+					  	<h6 class="count-text ">คนที่เลือกใช้บริการเรา</h6>
+					</div>
+				</div>
+				<div class="col">
+					<div class="counter">
+						<i class="fas fa-file-export"></i>
+						<h2 class="timer count-title count-number" data-to="257" data-speed="2000"></h2>
+						<h6 class="count-text ">กว่าโปรโมชั่นที่เสนอไปแล้ว</h6>
+					</div>
+				</div>
+				<div class="col">
+					<div class="counter">
+						<i class="	far fa-user"></i>
+						<h2 class="timer count-title count-number" data-to="126" data-speed="2000"></h2>
+						<h6 class="count-text ">กว่าเจ้าหน้าที่ ที่พร้อมให้บริการคุณ</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
 		
 	<!-- Modal -->
 	<div id="success_tic" class="modal fade" role="dialog">
@@ -406,6 +449,105 @@
 			  
 		});
 	</script>
+	<script>
+			(function ($) {
+				$.fn.countTo = function (options) {
+					options = options || {};
+					
+					return $(this).each(function () {
+						// set options for current element
+						var settings = $.extend({}, $.fn.countTo.defaults, {
+							from:            $(this).data('from'),
+							to:              $(this).data('to'),
+							speed:           $(this).data('speed'),
+							refreshInterval: $(this).data('refresh-interval'),
+							decimals:        $(this).data('decimals')
+						}, options);
+						
+						// how many times to update the value, and how much to increment the value on each update
+						var loops = Math.ceil(settings.speed / settings.refreshInterval),
+							increment = (settings.to - settings.from) / loops;
+						
+						// references & variables that will change with each update
+						var self = this,
+							$self = $(this),
+							loopCount = 0,
+							value = settings.from,
+							data = $self.data('countTo') || {};
+						
+						$self.data('countTo', data);
+						
+						// if an existing interval can be found, clear it first
+						if (data.interval) {
+							clearInterval(data.interval);
+						}
+						data.interval = setInterval(updateTimer, settings.refreshInterval);
+						
+						// initialize the element with the starting value
+						render(value);
+						
+						function updateTimer() {
+							value += increment;
+							loopCount++;
 							
+							render(value);
+							
+							if (typeof(settings.onUpdate) == 'function') {
+								settings.onUpdate.call(self, value);
+							}
+							
+							if (loopCount >= loops) {
+								// remove the interval
+								$self.removeData('countTo');
+								clearInterval(data.interval);
+								value = settings.to;
+								
+								if (typeof(settings.onComplete) == 'function') {
+									settings.onComplete.call(self, value);
+								}
+							}
+						}
+						
+						function render(value) {
+							var formattedValue = settings.formatter.call(self, value, settings);
+							$self.html(formattedValue);
+						}
+					});
+				};
+				
+				$.fn.countTo.defaults = {
+					from: 0,               // the number the element should start at
+					to: 0,                 // the number the element should end at
+					speed: 1000,           // how long it should take to count between the target numbers
+					refreshInterval: 100,  // how often the element should be updated
+					decimals: 0,           // the number of decimal places to show
+					formatter: formatter,  // handler for formatting the value before rendering
+					onUpdate: null,        // callback method for every time the element is updated
+					onComplete: null       // callback method for when the element finishes updating
+				};
+				
+				function formatter(value, settings) {
+					return value.toFixed(settings.decimals);
+				}
+			}(jQuery));
+			
+			jQuery(function ($) {
+			  // custom formatting example
+			  $('.count-number').data('countToOptions', {
+				formatter: function (value, options) {
+				  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+				}
+			  });
+			  
+			  // start all the timers
+			  $('.timer').each(count);  
+			  
+			  function count(options) {
+				var $this = $(this);
+				options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+				$this.countTo(options);
+			  }
+			});
+	</script>				
 </body>
 </html>
