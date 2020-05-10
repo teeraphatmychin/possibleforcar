4271864a36fb29e290b2dc6000e7705a2b19e74f
teeraphat <teeraphatmychin@gmail.com>
2020-05-10 13:35:41 +0700

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-use App\Http\Controllers\Controller;
 use Socialite;
+use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 
