92fdff705a3c6cdcbb9887c8a9efc8ab87584d63
teeraphat <teeraphatmychin@gmail.com>
2020-05-14 19:43:23 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        $this->middleware('admin')->except(['index']);
+        //$this->middleware('admin')->except(['index']);
     }
 
 
