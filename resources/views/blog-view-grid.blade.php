18d55c20ab41738742918895023451cf8730893c
teeraphat <teeraphatmychin@gmail.com>
2020-05-19 13:24:04 +0700

    Update blog-view-grid.blade.php

--- resources/views/blog-view-grid.blade.php
+++ resources/views/blog-view-grid.blade.php
@@ -56,3 +56,38 @@
 </body>
 </html>
 
+
+  <script type="text/javascript">
+	var page = 1;
+	$(window).scroll(function() {
+	    if($(window).scrollTop() + $(window).height() >= $(document).height()-50) {
+	        page++;
+	        loadMoreData(page);
+	    }
+	});
+
+	function loadMoreData(page){
+	  $.ajax(
+	        {
+	            url: '?page=' + page,
+	            type: "get",
+	            beforeSend: function()
+	            {
+	                $('.ajax-load').show();
+	            }
+	        })
+	        .done(function(data)
+	        {
+	            if(data.html == " "){
+	                $('.ajax-load').html("No more records found");
+	                return;
+	            }
+	            $('.ajax-load').hide();
+	            $("#post-data").append(data.html);
+	        })
+	        .fail(function(jqXHR, ajaxOptions, thrownError)
+	        {
+	             // alert('server not responding...');
+	        });
+	}
+</script>  
