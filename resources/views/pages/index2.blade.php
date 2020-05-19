3d48e24e3907f4efff2b61e8b2e34919872a8bfa
teeraphat <teeraphatmychin@gmail.com>
2020-05-19 15:42:20 +0700

    up

--- resources/views/pages/index2.blade.php
+++ resources/views/pages/index2.blade.php
@@ -8,9 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Favicons -->
-	<link rel="shortcut icon" href="{{ asset('frontend/boighor/boighors/images/favicon.ico')}}">
-	<link rel="apple-touch-icon" href="{{ asset('frontend/boighor/boighors/images/icon.png')}}">
-
+	
 	<!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
